0a77adb89a369f24db7d12898da68dd21addec51
Mathieu Ferster <mferster@users.noreply.github.com>
2015-05-19 17:29:57 -0400

    Create pirateTreasure.js

---
+++ pirateTreasure.js
@@ -0,0 +1,17 @@
+angular.module('pirateTreasure', [])
+	.controller('myController', ['$scope', '$http', function($scope, $http) {
+		$scope.loading = true;
+		$scope.getCoins = function(){
+			$scope.loading = false;
+			var url = "http://pirate.azurewebsites.net/api/Pirate/"+$scope.numberOfPirates;
+			$http.get(url).success(function(data, status, headers, config){
+					$scope.loading = true;
+					$scope.numberOfCoins = data;
+				})
+				.error(function (data, status, headers, config){
+					$scope.loading = true;
+					alert("Incorrect Input");
+					$scope.numberOfCoins = null;
+				});
+		};
+}]);
